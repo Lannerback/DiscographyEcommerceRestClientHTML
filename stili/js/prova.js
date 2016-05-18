e431f9b4921d3d0d74f4b4a236aede3bc65c0d69
Adriano D'Aguanno <adriano.daguanno96@gmail.com>
2016-05-18 13:11:19 +0200

    ajax request modified

--- stili/js/prova.js
+++ stili/js/prova.js
@@ -1,15 +1,29 @@
 $(document).ready(function(){    
   $.ajax({
-    type: 'GET',
-    dataType: 'json',
-    url: 'http://admin:password@localhost:8080/RestDiscographyEcommerce/api/admin/album/list',
-    success: function (data, textStatus, jqXHR){                             
-        $('#imgprova').attr('src',data[1].imagebase64);
-    },
-    error: function (jqXHR, textStatus, errorThrown) {
-        alert("Error " + textStatus + ", message:" + errorThrown);
-    },
-    complete: function () {
-    }
-  });
+          type: 'GET',
+          dataType: 'json',          
+          url: "http://localhost:8080/RestDiscographyEcommerce/api/admin/album/list",
+          beforeSend: function (xhr) {
+              xhr.setRequestHeader('Authorization',
+                      this.makeBaseAuth("admin", "password"));
+          },
+          success: function (data, textStatus, jqXHR) {
+              $('#imgprova').attr('src',data[1].imagebase64);
+              console.log(data);
+          },
+          error: function (jqXHR, textStatus, errorThrown) {
+              alert("Error " + textStatus + ", message:" + errorThrown);
+          },
+          complete: function () {
+
+          },
+          makeBaseAuth: function (user, pswd) {
+              var token = user + ':' + pswd;
+              var hash = "";
+              if (btoa) {
+                  hash = btoa(token);
+              }
+              return "Basic " + hash;
+          }
+     });    
 });
